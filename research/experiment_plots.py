9b12a751a7e3ce64e0051f5d8c79c9f523a7e75e
hayachan-mugi <hybs1000@gmail.com>
2020-08-27 14:28:39 +0900

    change the experiment_plots.py

--- research/experiment_plots.py
+++ research/experiment_plots.py
@@ -1,5 +1,22 @@
 import csv
 import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
+import codecs
 
-with open('vacant_T_Ba2F_12umSpacer_16scans_2.0cm.csv') as f:
-    print(f.read())
+file = "vacant_T_Ba2F_12umSpacer_16scans_2.0cm.csv"
+with open(file, "r", encoding = "Shift-JIS") as fileobj:
+    f = fileobj.drop(0,axis=0)
+    while True :
+        line = f.readline()[20:]
+        print(line)
+        break
+
+#print(wave_num)
+
+#with codecs.open("vacant_T_Ba2F_12umSpacer_16scans_2.0cm.csv", "r", "Shift-JIS", "ignore") as file:
+# data = pd.read_table(file, delimiter=",")
+
+#df = data.iloc[0:4,20:]
+#print(df)--#
+
