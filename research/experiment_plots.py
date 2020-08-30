97bad66b6565418e26e6e0566ace3da36e9729cd
hayachan-mugi <hybs1000@gmail.com>
2020-08-31 03:14:53 +0900

    change the experimnt_plots.py

--- research/experiment_plots.py
+++ research/experiment_plots.py
@@ -1,25 +1,75 @@
 import csv
-import pprint
 import pandas as pd
 import matplotlib.pyplot as plt
-import codecs
 import numpy as np
+import collections
+import math
+def flatten(l):
+    for el in l:
+        if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
+            yield from flatten(el)
+        else:
+            yield el
 
-file = "vacant_T_Ba2F_12umSpacer_16scans_2.0cm.csv"
+file = "BG_16scans_2.0cm_morning.csv"
+lines = []
+f = pd.read_csv(file,encoding='Shift-JIS')
 
-#fileobj = pd.read_csv(file,encoding='Shift-JIS')
-with open(file, "r", encoding = "Shift-JIS") as fileobj:
+f.columns=list('AB')
+d1 = f[f['A']=='XYDATA'].index
+d2 = np.array(d1) + 1
+d3 = f[f['A']=='##### Extended Information'].index
+
+d3 = np.array(d3) - 1
+
+XDATA = f.iloc[d2[0]:d3[0],0:1]
+YDATA = f.iloc[d2[0]:d3[0],1:2]
+
+x = np.array(XDATA)
+y = np.array(YDATA)
+x = [float(s) for s in x]
+y = [float(s) for s in y]
+x = np.array(x)
+y = np.array(y)
+
+#convert 赤外線
+h = x/y
+h = np.log(h)
+print(h)
+
+
+'''x = x.tolist()
+y = y.tolist()
+x = list(flatten(x))
+y = list(flatten(y)) 
+'''
+'''
+for i in range(len(x)):
+     a = x[i] + y[i]
+     a = np.append(a)
+print(a)
+'''
+plt.plot(x,h)
+plt.show()
+
+'''with open(file, "r", encoding = "Shift-JIS") as f:
+    rows = csv.reader(f)
+    print(type(rows))
+    print(rows)
+    for row in rows:
+        lines.append(row)
+'''
+    #d = lines[18:20,0:2]
+    #print(d)
 #fileobj.columns=list('AB')
 #d = fileobj['A'].isnull()
 #g = fileobj.iloc[:,:].isnull()
 #y=g.values
  #   for row in fileobj:
   #     print('a')
-    print(fileobj[0:15,:])
+
 #print(len(y))
 #if y.all() == False:
-    
-print('aa')
 
 #print(fileobj['A'].isnull())
 '''
@@ -42,5 +92,5 @@ print(df.values)
 # data = pd.read_table(file, delimiter=",")
 
 #df = data.iloc[0:4,20:]
-#print(df)--#
+#print(df)
 '''
